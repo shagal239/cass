456a288b168d0738576a3199b9571fca5b67ec63
Shagal.Arkady <shagal239@gmail.com>
2013-12-06 15:48:34 +0400

    new version of processor

--- RowProcessor/src/main/java/org/apache/cassandra/db/proc/RemoveColumnsRowProcessor.java
+++ RowProcessor/src/main/java/org/apache/cassandra/db/proc/RemoveColumnsRowProcessor.java
@@ -1,29 +1,25 @@
 package org.apache.cassandra.db.proc;
 
-import org.apache.cassandra.db.ColumnFamily;
-import org.apache.cassandra.db.ColumnFamilyStore;
-import org.apache.cassandra.db.DecoratedKey;
-import org.apache.cassandra.db.IColumn;
-import org.apache.cassandra.db.index.SecondaryIndex;
-import org.apache.cassandra.db.index.SecondaryIndexManager;
+import org.apache.cassandra.db.*;
 
 import java.util.Iterator;
+import java.util.Map;
 import java.util.Properties;
 
 
 public class RemoveColumnsRowProcessor implements IRowProcessor {
 
     private static final double PERCENT = 0.8;
-    private int gcBefore;
 
-    public RemoveColumnsRowProcessor(int gcBefore)
-    {
-        this.gcBefore = gcBefore;
-    }
+    public int gcBefore;
 
     @Override
     public void setConfiguration(Properties config)
     {
+        assert config.getProperty("class").equals("RemoveColumns") : config.getProperty("class") +" != "+"RemoveColumns";
+        for(Map.Entry entry : config.entrySet()){
+            System.err.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 
     @Override
@@ -53,19 +49,34 @@ public class RemoveColumnsRowProcessor implements IRowProcessor {
     public ColumnFamily process(DecoratedKey key, ColumnFamily columns,
                                 boolean incomplete)
     {
-        SecondaryIndexManager.Updater updater = SecondaryIndexManager.nullUpdater;
-        int size = columns.getColumnCount();
-        if(size > PERCENT * Integer.MAX_VALUE){
-            Iterator<IColumn> iter = columns.iterator();
-            for(int i = 0; i < size/4; i++){iter.next();}
 
-            for(int i = 0; i < size/2; i++){
-                IColumn column  = iter.next();
-                iter.remove();
-                updater.remove(column);
+        System.err.println("yeah we are here and we are ready to start");
+        System.err.println(key.key);
+        System.err.println(incomplete);
+
+
+
+        int size = columns.getSortedColumns().size();
+
+        if (size > PERCENT * 1000) {
+
+            System.err.println("size " + size);
+            System.err.println("we are startinf");
+            Iterator<IColumn> columnIterator = columns.getSortedColumns().iterator();
+            for(int i = 0; i < size/4; i++){
+                columnIterator.next();
             }
+            for(int i = 0; i < size/2 && columnIterator.hasNext(); i++){
+                Column column = (Column) columnIterator.next();
+                columns.remove(column.name());
 
+            }
+            System.err.println("we are here");
+            System.err.println("new size " + columns.getSortedColumns().size());
         }
-        return ColumnFamilyStore.removeDeleted(columns, gcBefore);
+
+
+
+        return columns;
     }
 }
