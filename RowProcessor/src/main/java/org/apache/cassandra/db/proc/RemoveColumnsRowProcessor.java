f4796cb07707113a3a994d589ba1aaf6a4d5451d
Shagal.Arkady <shagal239@gmail.com>
2013-12-07 17:27:34 +0400

    deleting via StorageProxy

--- RowProcessor/src/main/java/org/apache/cassandra/db/proc/RemoveColumnsRowProcessor.java
+++ RowProcessor/src/main/java/org/apache/cassandra/db/proc/RemoveColumnsRowProcessor.java
@@ -1,25 +1,36 @@
 package org.apache.cassandra.db.proc;
 
 import org.apache.cassandra.db.*;
+import org.apache.cassandra.db.filter.QueryPath;
+import org.apache.cassandra.service.StorageProxy;
+import org.apache.cassandra.thrift.ColumnPath;
+import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.thrift.UnavailableException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
+import java.util.concurrent.TimeoutException;
 
 
 public class RemoveColumnsRowProcessor implements IRowProcessor {
-
+    public static final int MAXROWMUTATIONS = 239;
+    private Logger logger = LoggerFactory.getLogger(RemoveColumnsRowProcessor.class);
     private static final double PERCENT = 0.8;
+    private int MAXSIZE;
 
+    public String table;
     public int gcBefore;
 
     @Override
     public void setConfiguration(Properties config)
     {
         assert config.getProperty("class").equals("RemoveColumns") : config.getProperty("class") +" != "+"RemoveColumns";
-        for(Map.Entry entry : config.entrySet()){
-            System.err.println(entry.getKey() + " " + entry.getValue());
-        }
+        assert config.containsKey("table");
+        assert config.containsKey("maxcolumncount");
+
+        table = config.getProperty("table");
+        MAXSIZE = Integer.parseInt(config.getProperty("maxcolumncount"));
     }
 
     @Override
@@ -58,21 +69,36 @@ public class RemoveColumnsRowProcessor implements IRowProcessor {
 
         int size = columns.getSortedColumns().size();
 
-        if (size > PERCENT * 1000) {
 
-            System.err.println("size " + size);
-            System.err.println("we are startinf");
+        if (size > PERCENT * MAXSIZE) {
+            logger.info("DADADADADADAD mi zashli v blok");
             Iterator<IColumn> columnIterator = columns.getSortedColumns().iterator();
-            for(int i = 0; i < size/4; i++){
+
+            for (int i = 0; i < size / 4; i++) {
                 columnIterator.next();
             }
-            for(int i = 0; i < size/2 && columnIterator.hasNext(); i++){
-                Column column = (Column) columnIterator.next();
-                columns.remove(column.name());
-
+            List<RowMutation> buffer = new ArrayList<RowMutation>();
+            for (int i = 0; columnIterator.hasNext() && (i < size / 2); i++) {
+                RowMutation rowMutation = new RowMutation(table, key.key);
+                ColumnPath path = new ColumnPath(columns.name());
+                IColumn column = columnIterator.next();
+                path.setColumn(column.name());
+                rowMutation.delete(new QueryPath(path), column.timestamp());
+                buffer.add(rowMutation);
+                if(buffer.size() >= MAXROWMUTATIONS){
+                    try {
+                        //logger.info("sending 100 RowMutations");
+                        StorageProxy.mutateBlocking(buffer, ConsistencyLevel.ALL);
+                        logger.info("we did it");
+                    } catch (UnavailableException e) {
+                        logger.error(e.getMessage());
+                    } catch (TimeoutException e) {
+                        logger.error(e.getMessage());
+                    }
+                    buffer.clear();
+                        logger.info("cleaned buffer "  +buffer.size());
+                }
             }
-            System.err.println("we are here");
-            System.err.println("new size " + columns.getSortedColumns().size());
         }
 
 
